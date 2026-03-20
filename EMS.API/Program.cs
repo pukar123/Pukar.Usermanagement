d367cfb10b97c2d39460e6d9f2b5939f96f515d8
pukarsubedi18 <pukarsubedi18@gmail.com>
2026-03-20 20:41:54 +1100

    Initial

--- EMS.API/Program.cs
+++ EMS.API/Program.cs
@@ -1,3 +1,6 @@
+using EMS.Domain.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.MigrationsAssembly(typeof(Program).Assembly.GetName().Name)));
+
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<AppDbContext>(name: "database");
 
 var app = builder.Build();
 
@@ -18,6 +29,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapHealthChecks("/health");
 app.MapControllers();
 
 app.Run();
